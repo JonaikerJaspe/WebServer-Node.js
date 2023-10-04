5979d91823be642919771c9d18bac7bc25d9c353
JonaikerJaspe <JonaikerJaspe@gmail.com>
2023-10-04 13:47:15 -0400

    otro cambio de /public

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 
 //Servir Contenido Estatico
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-	res.render("home", {
+	res.render("home.hbs", {
 		nombre: "Jonaiker Jaspe",
 		titulo: "Curso - Node.js",
 	});
 });
 app.get("/generic", (req, res) => {
-	res.render("generic", {
+	res.render("generic.hbs", {
 		nombre: "Jonaiker Jaspe",
 		titulo: "Curso - Node.js",
 	});
 });
 app.get("/elements", (req, res) => {
-	res.render("elements", {
+	res.render("elements.hbs", {
 		nombre: "Jonaiker Jaspe",
 		titulo: "Curso - Node.js",
 	});
